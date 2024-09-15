20541cf33f35f1a15208db1470aaf11c55aa7423
jvmaiaa <joaovictormaia482@gmail.com>
2024-09-15 14:59:09 -0300

    🚧Feat: permite a conexão com PostgreSQL e config variáveis de ambiente.

--- src/main/java/com/desafio/backend/infra/env/DataSourceConfig.java
+++ src/main/java/com/desafio/backend/infra/env/DataSourceConfig.java
@@ -1,4 +1,25 @@
 package com.desafio.backend.infra.env;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
 public class DataSourceConfig {
+
+    @Autowired
+    private Dotenv dotenv;
+
+    @Bean
+    public DataSource dataSource() {
+        return DataSourceBuilder.create()
+                .username(dotenv.get("DB_USERNAME"))
+                .password(dotenv.get("DB_PASSWORD"))
+                .url(dotenv.get("DB_URL"))
+                .build();
+    }
 }
