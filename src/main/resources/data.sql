30dabb6b06a4c7dfba8f52f2583977917c73a2b4
jvmaiaa <joaovictormaia482@gmail.com>
2024-09-22 23:52:17 -0300

    🐛BugFix: Comenta script com valores iniciais.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
-INSERT INTO public.tb_usuario (papel, login, senha)
-VALUES
-('CLIENTE', 'clienteLogin', '$2a$10$HgTFGqojdiAtQzRRtJGKqe487noQrrdCRFJPv9T7qREvfazeivQb6'),
-('FUNCIONARIO', 'funcionarioLogin', '$2a$10$HgTFGqojdiAtQzRRtJGKqe487noQrrdCRFJPv9T7qREvfazeivQb6'),
-('GERENTE', 'gerenteLogin', '$2a$10$HgTFGqojdiAtQzRRtJGKqe487noQrrdCRFJPv9T7qREvfazeivQb6'),
+--INSERT INTO public.tb_usuario (papel, login, senha)
+--VALUES
+--('CLIENTE', 'clienteLogin', '$2a$10$HgTFGqojdiAtQzRRtJGKqe487noQrrdCRFJPv9T7qREvfazeivQb6'),
+--('FUNCIONARIO', 'funcionarioLogin', '$2a$10$HgTFGqojdiAtQzRRtJGKqe487noQrrdCRFJPv9T7qREvfazeivQb6'),
+--('GERENTE', 'gerenteLogin', '$2a$10$HgTFGqojdiAtQzRRtJGKqe487noQrrdCRFJPv9T7qREvfazeivQb6'),
