1b4c84f526ea9da333a7956425a326ef754ac56e
jvmaiaa <joaovictormaia482@gmail.com>
2024-10-20 03:02:48 -0300

    🚧Feat: Implementa primeira versão do docker-compose.

--- src/main/java/com/desafio/backend/infra/env/ConfigVerifier.java
+++ src/main/java/com/desafio/backend/infra/env/ConfigVerifier.java
@@ -1,16 +1,16 @@
-package com.desafio.backend.infra.env;
-
-import io.github.cdimascio.dotenv.Dotenv;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class ConfigVerifier {
-
-    @Bean
-    public Dotenv dotenv() {
-        return Dotenv.configure()
-                .filename(".env.properties")
-                .load();
-    }
-}
+//package com.desafio.backend.infra.env;
+//
+//import io.github.cdimascio.dotenv.Dotenv;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class ConfigVerifier {
+//
+//    @Bean
+//    public Dotenv dotenv() {
+//        return Dotenv.configure()
+//                .filename(".env.properties")
+//                .load();
+//    }
+//}
